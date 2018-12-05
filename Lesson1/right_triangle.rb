1dbf98b041a2bc1e26c5dafe8b617cade82f1f7e
KDS <ds.krav@gmail.com>
2018-12-05 11:34:57 +0800

    Сorrection of comments

--- Lesson1/right_triangle.rb
+++ Lesson1/right_triangle.rb
@@ -1,11 +1,12 @@
 sides = []
 def right_triangle(sides)
-
   return "Треугольник равносторонний, но не является прямоугольным." if sides.uniq.length == 1
 
   a, b, c = sides.sort # Порядок ввода межет быть произвольным.
 
-  if a == b && a**2 + b**2 == c**2
+  compExp = a**2 + b**2 == c**2
+
+  if a == b && compExp
     "Треугольник является прямоугольным и равнобедренным."
   elsif a**2 + b**2 == c**2
     "Треугольник является прямоугольным."
@@ -15,10 +16,8 @@ def right_triangle(sides)
 end
 
 3.times do
-
   puts "Пожалуйста, введите сторону треугольника: "
   sides << gets.to_f
-  
 end
 
 
